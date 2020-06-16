37fe63981c9bbaf3c07996d523c96bf6603ba9c9
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2020-06-16 09:49:12 +0300

    algorithms: fibonacci (rust)

--- src/Algorithms/Math/Fibonacci/fibonacci.rs
+++ src/Algorithms/Math/Fibonacci/fibonacci.rs
@@ -1,3 +1,4 @@
+// Recursive approach
 #[cfg(test)]
 pub fn fibonacci(n: u32) -> u32 {
     match n {
@@ -5,6 +6,29 @@ pub fn fibonacci(n: u32) -> u32 {
         1 => 1,
         _ => fibonacci(n - 1) + fibonacci(n - 2),
     }
+}
+
+// Iterative approach
+struct Fib {
+    c: u32,
+    n: u32,
+}
+
+impl Iterator for Fib {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        let n = self.c + self.n;
+        self.c = self.n;
+        self.n = n;
+
+        Some(self.c)
+    }
+}
+
+#[allow(dead_code)]
+fn fib() -> Fib {
+    Fib { c: 0, n: 1 }
 }
 
 #[cfg(test)]
@@ -15,5 +39,11 @@ mod tests {
             55,
             crate::Algorithms::Math::Fibonacci::fibonacci::fibonacci(10)
         );
+
+        let res = crate::Algorithms::Math::Fibonacci::fibonacci::fib()
+            .skip(9)
+            .next()
+            .unwrap();
+        assert_eq!(55, res);
     }
 }
