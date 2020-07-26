e9aa9089984f9e56eedb93e3747e6c328b360179
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2020-07-26 21:43:16 +0300

    chore: add tests

--- src/Algorithms/Lists/Two_Numbers_Sum/two-numbers-sum.test.ts
+++ src/Algorithms/Lists/Two_Numbers_Sum/two-numbers-sum.test.ts
@@ -11,5 +11,13 @@ describe("Two numbers sum", () => {
     expect(solution([1, -2, 1, 0, 5], 0)).toBe(-1);
     expect(solution([1, 2, 3, 9], 8)).toBe(-1);
     expect(solution([1, 2, 4, 4], 8)).toEqual([4, 4]);
+    expect(solution([2, 4], 1)).toBe(-1);
+    expect(solution([2, 4], 6)).toEqual([2, 4]);
+    expect(solution([3, 8], 6)).toBe(-1);
+    expect(solution([3, 8, 3], 6)).toEqual([3, 3]);
+    expect(solution([1, 2, 3, 4, 5, 6], 7)).toBeTruthy();
+    expect(solution([4, 3, 2], 5)).toBeTruthy();
+    expect(solution([6], 6)).toBe(-1);
+    expect(solution([], 2)).toBe(-1);
   });
 });
