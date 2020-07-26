92505f5307c508884ac4c1b4d4caae987158984c
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2020-07-26 11:33:52 +0300

    algorithms: merge arrays (typescript)

---
+++ src/Algorithms/Lists/Merge_Arrays/README.md
@@ -0,0 +1,10 @@
+# Merge Arrays
+
+Given two sorted arrays of integers. Write a function to merge our arrays of orders into one sorted array.
+
+```javascript
+const arrayOne = [3, 4, 6, 10, 11, 15];
+const arrayTwo = [1, 5, 8, 12, 14, 19];
+
+mergeArrays(arrayOne, arrayTwo); // [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+```
---
+++ src/Algorithms/Lists/Merge_Arrays/merge-arrays.ts
@@ -0,0 +1,23 @@
+// O(n) time
+// O(n) space
+const solution = (arrayOne: number[], arrayTwo: number[]) => {
+  let indexOne = 0;
+  let indexTwo = 0;
+
+  const result = [];
+  while (indexOne < arrayOne.length || indexTwo < arrayTwo.length) {
+    if (!arrayTwo[indexTwo] || arrayOne[indexOne] <= arrayTwo[indexTwo]) {
+      result.push(arrayOne[indexOne]);
+      indexOne++;
+    } else {
+      result.push(arrayTwo[indexTwo]);
+      indexTwo++;
+    }
+  }
+
+  return result;
+};
+
+export const solutions = {
+  Solution: solution,
+};
