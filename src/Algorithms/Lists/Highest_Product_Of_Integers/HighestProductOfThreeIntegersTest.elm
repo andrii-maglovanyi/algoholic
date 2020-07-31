dbbf6f4454c1b374070ba08f0294f1ec58ea2368
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2020-07-31 15:33:07 +0300

    add elm testing

--- src/Algorithms/Lists/Highest_Product_Of_Integers/HighestProductOfThreeIntegersTest.elm
+++ src/Algorithms/Lists/Highest_Product_Of_Integers/HighestProductOfThreeIntegersTest.elm
@@ -7,5 +7,8 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    test "Highest product of three integers"
-        (\_ -> Expect.equal 24 (highestProductOfThreeIntegers [ 1, 2, 3, 4 ]))
+    test "Highest product of three integers" <|
+        \() ->
+            [ [ 1, 2, 3, 4 ], [ 6, 1, 3, 5, 7, 8, 2 ], [ -5, 4, 8, 2, 3 ], [ -10, 1, 3, 2, -10 ], [ -5, -1, -3, -2 ], [ 1, 10, -5, 1, -100 ] ]
+                |> List.map highestProductOfThreeIntegers
+                |> Expect.equal [ 24, 336, 96, 300, -6, 5000 ]
